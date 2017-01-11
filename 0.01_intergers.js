31ded6204dfcff2cc27d6ac1461b5f8ff37495da
seeker.of.light <seekeroflight75@yahoo.com>
2017-01-11 11:13:35 -0500

    Added intergers and variables in js

---
+++ 0.01_intergers.js
@@ -0,0 +1,44 @@
+console.log(1.2);
+
+console.log(57+1098);
+console.log(83*227);
+console.log(158463/-7);
+console.log(7-2);
+console.log(26 % 10624);
+
+
+
+//--------------------------------------------------------
+//--------------------------------------------------------
+//------------- Challenge: Integers--------------
+//--------------------------------------------------------
+//--------------------------------------------------------
+
+/*CHALLENGE: Bronze 
+Create four integers: your age, birth year, graduation year, and number of brothers and sisters. 
+Print them to the console. Put comments above each one to identify.
+*/
+
+/* CHALLENGE: Silver
+Using the modulus, write some simple modulus equations that will yield the numbers below. Use only two numbers:
+1, 2, 12
+*/
+
+
+/* CHALLENGE: Gold
+Research how to create an exponent.
+*/
+
+console.log(41);
+console.log(1975);
+console.log(1994);
+console.log(1);
+console.log("brother");
+
+/*silver*/
+
+console.log(7 % 2);
+console.log(11 % 3);
+console.log(30 % 18);
+console.log(3e1);
+
---
+++ 0.02_variables.js
@@ -0,0 +1,113 @@
+//this variable is a string type
+var firstname = "Doug";
+var lastname = "Kiel";
+var fullname = firstname +" "+ lastname
+//concatenation (this is where you can build or combine strings)
+console.log(firstname);
+console.log(firstname +" "+ lastname);
+console.log(fullname);
+
+//empty variables
+
+console.log(num)
+var num
+//variables and console.logs when swapped do not change how the computer reads the code.
+
+var petsName = "";
+
+//conventional variable namings
+//camel case (petsName) - first word is lower cased the next words are uppercase
+
+var packersScoreThisWeekend = 38;
+
+//snake case - each word is connected with an _
+var packers_score_from_last_weekend = 35;
+
+//let
+//if (true){
+//	let
+//}
+
+//shorthand operators
+
+var myage = 41;
+//console.log(1 += 1); //1 = 1 + 1
+console.log(myage += 1);
+console.log(myage /= 2);
+console.log(myage *= 2);
+console.log(myage -= 1);
+
+
+//--------------------------------------------------------
+//--------------------------------------------------------
+// -------------- Challenge: Variables-----------
+//--------------------------------------------------------
+//--------------------------------------------------------
+
+/*
+CHALLENGE: Bronze
+Come up with five different variables. Think of 
+real world uses for integers and create variables based on 
+those things. A few examples might include: 
+*/
+
+/*var myBankAccount = 2000000; //No dollar sign, we'll discuss how to add this later.
+var numberOfHotDogsKobyashiAte = 39; //Check this stat. Not that's how many he ate.
+
+console.log(myBankAccount);
+console.log("Kobyashi:" + numberOfHotDogsKobyashiAte); *//* <----String concatenation
+
+/*-----------------------------------------------------------*/
+
+//CHALLENGE: Silver
+ //Make a var called myNumber
+//Set it to 0.
+//Use shorthand to add 10 to it.
+//Then use shorthand to subtract 1.
+//Use shorthand to multiply by 9.
+//Use shorthand to divide by 7.
+//Just for practice, print myNumber to the console between each operation.
+/*************************************************************
+	 Operator ***** Example ***** Equivalent *****
+    	+=		|    a += b   |    a = a + b   | 
+    	-=		|    a -= b   |    a = a - b   | 
+    	*=		|    a *= b   |    a = a * b   | 
+    	/=		|    a /= b   |    a = a / b   | 
+    	%=		|    a %= b   |    a = a % b   |   
+              *****         *****            *****
+*************************************************************/
+
+/*-----------------------------------------------------------*/
+//CHALLENGE: Gold
+//Challenge #3 - Lifetime Supply
+// Ever wonder how much a "lifetime supply" of your favorite snack is? Wonder no more!
+// Store your current age into a variable.
+// Store a maximum age into a variable.
+// Store an estimated amount of glasses of water per day (as a number).
+// Calculate how many glasses of water you would need for the rest of your life.
+// Output the result to the screen like so: "You will need 5 million glasses to last you until the ripe old age of X".
+
+var yearsinBerlin = 10;
+var myyearsinKorea = 4;
+var yearsinMuncie = 8;
+var myBankAccount = 20;
+var alive = 41;
+console.log(yearsinBerlin);
+console.log(myyearsinKorea);
+console.log(yearsinMuncie);
+console.log(myBankAccount);
+console.log(alive);
+console.log("--------------------------------------");
+console.log("Silver Challenge")
+var myNumber = 0;
+console.log(myNumber += 10);
+console.log(myNumber -= 1);
+console.log(myNumber *= 9);
+console.log(myNumber /= 7);
+console.log("--------------------------------------");
+console.log("Gold Challenge")
+var maxAge = 100;
+var numPerDay = 
+
+
+
