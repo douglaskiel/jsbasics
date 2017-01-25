969c0047b462047cda030e4fd833142999ddafe6
seeker.of.light <seekeroflight75@yahoo.com>
2017-01-25 17:27:13 -0500

    added if-else

--- 1stbuilds/0.06_if_else.js
+++ 1stbuilds/0.06_if_else.js
@@ -25,41 +25,53 @@ if (elevatorStuckAndWeAreOnIt){
 	console.log("I'm going to the Bathroom.")
 }
 
-//--------------------------------------------------------
-//--------------------------------------------------------
-// ------------Challenges: If/Else Statements ----------
-//--------------------------------------------------------
-//--------------------------------------------------------
+//-----------------------------------------------
+//-----------------------------------------------
+//---------Challenges: If/Else Statements -------
+//-----------------------------------------------
+//-----------------------------------------------
 
-/*-----------------------------------------------------------*/
+/*---------------------------------------------*/
 //CHALLENGE: Bronze
 
-//WRITE YOUR OWN CONDITIONAL USING BOOLEANS AND LOGICAL OPERATORS.
+//WRITE YOUR OWN CONDITIONAL USING BOOLEANS 
+// AND LOGICAL OPERATORS.
 //Come up with a simple example
 
-//Challenge: Silver
-//Create a simple login console message.
-// create 2 variables 1 for your users firstName, 2 for the boolean condition
-// If login is true then print "You're Logged In!"
-//Else if login is false then print "Please Sign In"
+// Challenge: Silver
+// Create a simple login console message.
+// create 2 variables 1 for your users firstName, 
+// for the boolean condition
+// If login is true then print 
+// "You're Logged In!"
+// Else if login is false then print 
+// "Please Sign In"
 
 
-//CHALLEGE: Gold
-//Set up a var with bankAccount and a var with debt.
-//Also set up a difference var.
-//Do some logic to show whether or not you will pay off your debt or prints out a message if you have extra money.
+// CHALLEGE: Gold
+// Set up a var with bankAccount 
+// and a var with debt.
+// Also set up a difference var.
+// Do some logic to show whether 
+// or not you will pay off your debt 
+// or prints out a message if you 
+// have extra money.
 
 var isLoggedin = true;
 var firstName = "Doug";
 var password = true;
 var notPassword = false;
 
-if (login){
+if (isLoggedin === false){
 	console.log("Welcome! " + firstName);
 }else{
 	console.log("Sorry, try again.")
 }
 
+var abbyHatesTrump = true
+var abbyMakesMeHappy = true
+var Presbyterian = false
+var Methodist = false
 
 
 
